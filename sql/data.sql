b4566203648de44454f052e5ded6ad8ce6d60354
IndraniGhonge <indrani.ghonge.btech2024@sitpune.edu.in>
2026-04-21 12:58:37 +0530

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,5 +1,65 @@
 USE alatska;
 
+CREATE TABLE Country (
+    CountryID INT PRIMARY KEY,
+    CountryName VARCHAR(50)
+);
+
+CREATE TABLE City (
+    CityID INT PRIMARY KEY,
+    CityName VARCHAR(50),
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Country(CountryID)
+);
+
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Email VARCHAR(50),
+    Password VARCHAR(50)
+);
+create table TouristPlace_Before (
+    PlaceID int,
+    PlaceName varchar(50),
+    Category varchar(100),
+    CityID int
+);
+CREATE TABLE TouristPlace (
+    PlaceID INT PRIMARY KEY,
+    PlaceName VARCHAR(50),
+    Category VARCHAR(30),
+    CityID INT,
+    FOREIGN KEY (CityID) REFERENCES City(CityID)
+);
+
+CREATE TABLE Accommodation (
+    AccommodationID INT PRIMARY KEY,
+    AccommodationName VARCHAR(50),
+    Type VARCHAR(30),
+    PricePerNight INT,
+    CityID INT,
+    FOREIGN KEY (CityID) REFERENCES City(CityID)
+);
+
+CREATE TABLE Booking (
+    BookingID INT PRIMARY KEY,
+    BookingDate DATE,
+    UserID INT,
+    AccommodationID INT,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (AccommodationID) REFERENCES Accommodation(AccommodationID)
+);
+
+CREATE TABLE Review (
+    ReviewID INT PRIMARY KEY,
+    Rating INT,
+    Comment VARCHAR(100),
+    UserID INT,
+    PlaceID INT,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (PlaceID) REFERENCES TouristPlace(PlaceID)
+);
+
 INSERT INTO Country VALUES
 (1,'India'),
 (2,'Italy'),
@@ -20,6 +80,15 @@ INSERT INTO Users VALUES
 (2,'Ayush','ayush@gmail.com','pass456'),
 (3,'Indrani','indrani@gmail.com','pass789'),
 (4,'Jhanvi','jhanvi@gmail.com','pass999');
+
+INSERT INTO TouristPlace_Before VALUES
+(1,'Gateway of India','Historical',2),
+(2,'Shaniwar Wada','Historical',1),
+(3,'Colosseum','Historical',3),
+(4,'Grand Canal','Adventure',4),
+(5,'Shibuya Crossing','Adventure',5),
+(6,'Osaka Castle','Historical',6),
+(7,'Helsinki Cathedral','Historical, Religious',7);
 
 INSERT INTO TouristPlace VALUES
 (1,'Gateway of India','Historical',2),
